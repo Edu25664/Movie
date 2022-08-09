017f43dad01ade74ee6b0db08a5d625657f1a7bc
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-09 09:29:36 -0300

    create struct

---
+++ backend/src/main/java/com/edu25664/DSmovie/DSmovieApplication.java
@@ -0,0 +1,13 @@
+package com.edu25664.DSmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edu25664/DSmovie/DSmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu25664.DSmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
