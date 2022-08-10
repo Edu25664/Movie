0319ae3065f7cfb3df8a7da23de2a8c996a00ac4
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-09 22:49:40 -0300

    domain model, database seed

---
+++ backend/src/main/java/com/edu25664/DSmovie/entities/ScorePK.java
@@ -0,0 +1,33 @@
+package com.edu25664.DSmovie.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Embeddable
+public class ScorePK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public ScorePK(){}
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
